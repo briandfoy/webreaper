abfe8638fe19eb9afd6766a2bcb822c05e0398c4
brian d foy <brian.d.foy@gmail.com>
2015-09-08 01:16:27 -0400

    Add =encoding to all pod files

--- lib/App/webreaper.pm
+++ lib/App/webreaper.pm
@@ -22,6 +22,8 @@ use Socket;
 use URI;
 use YAML;
 
+=encoding utf8
+
 =head1 NAME
 
 webreaper -- download a web page and its links
@@ -194,7 +196,7 @@ Show progress information. This is the same as the -v switch.
 
 =item WEBREAPER_DIR
 
-Store downloads in this directory.  Script uses the current 
+Store downloads in this directory.  Script uses the current
 working directory if this directory does not exist.  This is
 the same as the -D switch.
 
@@ -271,7 +273,7 @@ getopts('a:dD:fh:n:N:p:r:s:tu:vz', \%opts);
 my $Debug   = defined $opts{d} || $ENV{WEBREAPER_DEBUG} || 0;
 my $Verbose = defined $opts{v} || $ENV{WEBREAPER_VERBOSE} || $Debug || 0;
 
-my $directory = $opts{D} || $ENV{WEBREAPER_DIR} || 
+my $directory = $opts{D} || $ENV{WEBREAPER_DIR} ||
 	do { print "Using current working directory\n" if $Verbose; cwd };
 
 die "Could not change directory to $directory: $!" unless chdir $directory;
@@ -317,7 +319,7 @@ if( defined $opts{r} and $opts{r} )
 	print "Referer host is $referer_host\n"         if $Debug;
 	$Allowed{ $referer_host } = 1;
 	}
-	
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 my $User_agent = $opts{a} || $ENV{WEBREAPER_UA} ||
 	q|Mozilla/4.5 (compatible; iCab 2.9.7; Macintosh; U; PPC; Mac OS X)|;
@@ -356,35 +358,35 @@ URL: while( @start )
 		}
 
 	$Seen{ $url_string }++;
-			
+
 	printf "[%5d] %s ... ", $count++, $url_string if $Verbose;
 
 	my $request   = make_request( $url );
-	
+
 	my $response  = $UA->request( $request );
-	
+
 	my( $base, $type, $data ) = process_response( $response );
-	
+
 	next URL unless( defined $base and defined $type );
-	
+
 	extract_links( $data, $base, $url, \@start ) if( $type eq 'text/html' );
 
 	last URL if stop();
-	
+
 	whoa() if defined $opts{'s'};
  	}
 $Stats{stop} = Benchmark->new;
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 print_summary() if $Verbose;
-	
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 tar() if $opts{t};
 zip() if $opts{z};
-	
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
- # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+ # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 sub stop
 	{
 	my $stop = do {
@@ -402,8 +404,8 @@ sub stop
 			{
 			0
 			}
-		};	
-	
+		};
+
 	return $stop
 	}
 
@@ -412,18 +414,18 @@ sub init_stats
 	{
 	@Stats{ qw(stored_files requests) } = ( 0, 0 );
 	}
-	
+
 sub extract_links
 	{
 	my( $data, $base, $referer, $urls ) = @_;
-	
+
 	print_debug( "Base is $base" ) if $Debug;
 	my $extor = HTML::SimpleLinkExtor->new( $base );
 	$extor->parse( $$data );
 
 	my @links = $extor->links;
 	print_debug( "Found " . @links . " links\n" ) if $Debug;
-	
+
 	use Data::Dumper;
 	print_debug( Dumper( \%Allowed ) ) if $Debug;
 
@@ -433,7 +435,7 @@ sub extract_links
 			not exists $Seen{ $_->[1] }   and
 			exists $Allowed{ $_->[0] }    and
 			not $_->[1] =~ m/^javascript/ and
-			$_->[1]->path =~ m/^\Q$Path/ 
+			$_->[1]->path =~ m/^\Q$Path/
 			}
 		map {
 			eval {
@@ -444,48 +446,48 @@ sub extract_links
 			} $extor->links;
 
 	print_debug( "Found " . @$urls . " links that I kept\n" ) if $Debug;
-			
+
 	print "Queue is now " . @$urls . "\n" if $Debug;
 	}
-	
+
 sub process_response
 	{
 	my $response = shift;
-	
+
 	my $final_url = $response->request->uri->canonical->as_string;
 
 	$Seen{ $final_url }++;
 	print_debug( "Final is [$final_url]\n" ) if $Debug;
 
 	return if $final_url =~ m/^file:/;
-	
+
 	my $file = get_store_name( $final_url );
-	
+
 	return if( -e $file && -s $file );
-	
+
 	my $data   = $response->content_ref;
 	my $code   = $response->code;
 	my $type   = $response->content_type;
 	my $server = $response->server;
-	
+
 	print "\n\tServer is $server ... " if $Debug;
 	print "$code\n" if $Verbose;
-	
+
 	$Stats{servers}{$server}++;
 	$Stats{codes}{$code}++;
-	
+
 	return if $response->is_error;
-	
-	# if Not-Modified, we don't store the file but we need to 
+
+	# if Not-Modified, we don't store the file but we need to
 	# get the links from the stored version, because those
 	# resources might have changed
 	store( $data, $file ) if $file;
-	
+
 	my $base = $response->base;
-	
+
 	return( $base, $type, $data );
 	}
-	
+
 sub print_summary
 	{
 	my $rule = "-" x 73 . "\n";
@@ -494,38 +496,38 @@ sub print_summary
 	my $Time = timestr( timediff( @Stats{ qw(stop start) } ) );
 
 	print "$FindBin::Script: $Time\n";
-	
+
 	my( $magnitude, $units ) = convert( $Stats{stored_bytes} );
-	printf "\trequested %d urls\n", $Stats{requests}; 
-	printf "\tstored %d files, %.2f %s\n", $Stats{stored_files}, 
+	printf "\trequested %d urls\n", $Stats{requests};
+	printf "\tstored %d files, %.2f %s\n", $Stats{stored_files},
 		$magnitude, $units;
-	
+
 	foreach my $code ( sort { $a <=> $b } keys %{ $Stats{codes} } )
 		{
 		my $reason = status_message( $code );
 		printf "%5d: %d %-20s\n", $Stats{codes}{$code}, $code, $reason;
 		}
 	print $rule;
-	
+
 	}
 
 sub convert
 	{
 	my $number = shift;
 	my @units = qw(bytes kB MB GB);
-	
+
 	print "Number is $number\n" if $Debug;
 	my $nearest = floor( log( $number ) / log( 1024 ) );
 	print "Floor is $nearest\n" if $Debug;
-	
+
 	foreach my $index ( 1 .. $nearest )
 		{
 		$number /= 1024;
 		}
-		
+
 	return ( $number, $units[$nearest] )
 	}
-	
+
 sub whoa
 	{
 	my $sleep = int rand( $opts{'s'} + 3 );
@@ -537,17 +539,17 @@ sub tar
 	{
 	eval "use Archive::Tar";
 	if( $@ ) { carp "You need Archive::Tar to create tar archives"; return }
-	
+
 	print "Domains is @Domains\n" if $Debug;
-	
+
 	my @files = ();
-	
+
 	find({
 		no_chdir => 1,
 		wanted   => sub { push @files, $_ if -f $_ },
 		}, @Domains );
-		
-		
+
+
 	my $compression = eval "IO::Zlib" ? 9 : 0;
 	my $extension   = $compression ? 'tgz' : 'tar';
 	Archive::Tar->create_archive( "$Domains[0].$extension", 9, @files );
@@ -557,22 +559,22 @@ sub zip
 	{
 	eval "use Archive::Zip";
 	if( $@ ) { carp "You need Archive::Zip to create zip archives"; return }
-	
+
 	my $zip = Archive::Zip->new();
 
 	foreach my $domain ( @Domains )
 		{
 		$zip->addTree( $domain );
 		}
-		
+
 	$zip->writeToFileNamed( "$Domains[0].zip" );
 	}
-		
+
 sub add_allowed_domain
 	{
 	my $domain = shift;
-	
-	$Allowed{$domain}++;	
+
+	$Allowed{$domain}++;
 
 	if( $domain =~ m/(?:[012]?\d\d?)(?:\.[012]?\d\d?){1,3}/ )
 		{
@@ -582,31 +584,31 @@ sub add_allowed_domain
 		print "Matched IP address [$domain|$host]\n";
 		$domain = $host;
 		}
-	
-	$domain;		
+
+	$domain;
 	}
-	
+
 sub make_request
 	{
 	my $url = shift;
-	
+
 	my $url_o = ref $url ? $url : URI->new( $url );
 	my $host = $url_o->host;
-		
+
 	my $store_name = File::Spec->rel2abs( get_store_name( $url_o->as_string ) );
-	
+
 	#print "Store name is $store_name\n";
-	
+
 	if( -e $store_name && -s $store_name )
 		{
 		print "Using localfile: $store_name\n";
 		$url_o = URI->new( "file://localhost/$store_name" );
 		}
-	
+
 	my $request = HTTP::Request->new( GET => $url_o );
-	
+
 	$request->authorization_basic( $opts{u}, $opts{p} ) if $authorization;
-	
+
 	$request->referer( "$Referers{$url}" ) if defined $Referers{$url};
 
 	$request->header( 'Accept-Language' => 'en'        );
@@ -614,15 +616,15 @@ sub make_request
 	$request->header( 'Accept'          => '*/*'       );
 	$request->header( 'Host'            => $host       );
 	$request->header( 'User-Agent'      => $User_agent );
-	
+
 	$Stats{requests}++;
-	
+
 	return $request;
 	}
 
 # XXX: break this into a function that determines the filename
-# XXX: store should remember the directories it creates so it 
-# can tar those later	
+# XXX: store should remember the directories it creates so it
+# can tar those later
 # XXX: store needs to remember how many bytes it wrote
 sub get_store_name
 	{
@@ -642,35 +644,35 @@ sub get_store_name
 		my $name = basename( $path );
 		return catfile( $domain, $name );
 		}
-			
+
 	if( $path =~ m|/$| )
 		{
 		print_debug( "Skipping path that looks like directory [$path]" )
 			if $Debug;
 		return;
 		}
-	
+
 	$path =  catfile( $domain, $path );
-	
+
 	print_debug( "Store path is [$path]" ) if $Debug;
-	
+
 	return $path;
 	}
-	
+
 sub store
  	{
 	my $data_ref = shift;
 	my $file     = shift;
 
 	print_debug( "Saving [$file]" ) if $Debug;
-	
+
 	if( -d $file )
 		{
-		print_debug( "Error: file path is already a directory [ $file ]" ) 
+		print_debug( "Error: file path is already a directory [ $file ]" )
 			if $Debug;
 		return;
 		}
-		
+
 	my $dir = dirname $file;
 	print_debug( "Directory is $dir" ) if $Debug;
 
@@ -678,7 +680,7 @@ sub store
 
 	if( -e $dir and not -d $dir )
 		{
-		print_debug( "Error: Removing file that should be a dir [$dir]" ) 
+		print_debug( "Error: Removing file that should be a dir [$dir]" )
 			if $Debug;
 		unlink $dir;
 		}
@@ -686,26 +688,26 @@ sub store
 		{
 		$Directories{$dir}++;
 		}
-		
+
 	eval { mkpath unless -e $dir };
 	if( $@ )
 		{
-		print_debug( "Error: mkpath could not make $dir: $@" ) 
+		print_debug( "Error: mkpath could not make $dir: $@" )
 			if $Debug;
 		return;
 		}
-	
+
 	my $fh;
 	unless( open $fh, "> $file" )
 		{
-		print_debug( "Could not open file [$file]: $!" ) 
+		print_debug( "Could not open file [$file]: $!" )
 			if $Debug;
 		return;
 		}
 
 	print $fh $$data_ref;
 	close $fh;
-	
+
 	$Stats{stored_bytes} += length $$data_ref;
 	$Stats{stored_files}++;
 	}
